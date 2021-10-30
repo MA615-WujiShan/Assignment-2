fd6be2705721a48e444ec3028ea0fb657d22fd91
newbieDX <chenxubu@bu.edu>
2021-10-30 18:22:41 -0400

    Modify visualization functions

--- visualization_function.R
+++ visualization_function.R
@@ -1,4 +1,4 @@
-# dv function is to compare fertility and life expetancy of female between two countries
+# dv function is to compare fertility and life expectancy of female between two countries
 dv <- function(data, country1, country2){
   data1 <- data %>%
     filter(country == country1)
@@ -8,14 +8,21 @@ dv <- function(data, country1, country2){
                  data.frame(country = data2$country, life = data2$life, fertility = data2$fertility))
   ggplot(graph, mapping = aes(x = life, y = fertility, 
                               group = country, color = country, shape = country)) +
-    geom_line()
+    geom_line() +
+    xlim(25,90) +
+    ggtitle("Per woman life expectancy vs. Per women total fertility") +
+    xlab("Per woman life expectancy(year)") + ylab("Per woman total fertility") +
+    geom_dl(aes(label = country), method = list(dl.combine("first.points")), cex = 0.8) 
 }
 
-# dv2 is to explore the the correlation of life expentancy of female and fertility in the world in these years
+# dv2 is to explore the the correlation of life expectancy of female and fertility in the world in these years
 dv2 <- function(data){
   # data = fertility_life
   data %>%
     count(life, fertility) %>%
     ggplot(aes(x = life, y = fertility)) + 
-    geom_tile(mapping = aes(fill = n))
+    ggtitle("Per woman life expectancy vs. Per women total fertility") +
+    xlab("Per woman life expectancy(year)") + ylab("Per woman total fertility") +
+    geom_tile(mapping = aes(fill = n)) +
+    geom_smooth(method = 'gam', aes(x = life, y = fertility), formula = y ~ s(x, bs = "cs"))
 }
